5c85351038243f3cc7f6f04d6b47d91d394e71d4
Ishab <43892687+ishabahmed@users.noreply.github.com>
2023-05-30 18:25:25 +0100

    Minor reformat and refactor

--- js/script.js
+++ js/script.js
@@ -1,89 +1,83 @@
+/**
+ * Calculator class to handle calculator operations
+ */
 class Calculator {
     constructor() {
-        this.displayValue = "0";
+        this.displayValue = '0';
         this.pendingOperand = null;
         this.pendingOperator = null;
         this.initialize();
     }
 
     initialize() {
-        this.display = document.querySelector(".calculator__display-text");
+        this.display = document.querySelector('.calculator__display-text');
         this.display.textContent = this.displayValue;
         this.setEventListeners();
     }
 
     setEventListeners() {
-        const buttons = document.querySelectorAll(".calculator__button");
+        const buttons = document.querySelectorAll('.calculator__button');
         buttons.forEach(button => {
-            button.addEventListener("click", () => {
-                this.handleInput(button);
-            });
+            button.addEventListener('click', () => this.handleInput(button));
         });
     }
 
     handleInput(button) {
-        const buttonType = button.classList.value;
-
-        if (buttonType.includes("operand")) {
-            this.handleOperandInput(button);
-        } else if (buttonType.includes("operator")) {
-            this.handleOperatorInput(button);
-        } else if (buttonType.includes("control")) {
-            this.handleControlInput(button);
+        const { classList, textContent, id } = button;
+        if (classList.contains('calculator__button--operand')) {
+            this.handleOperandInput(textContent, id);
+        } else if (classList.contains('calculator__button--operator')) {
+            this.handleOperatorInput(textContent);
+        } else if (classList.contains('calculator__button--control')) {
+            this.handleControlInput(textContent, id);
         }
 
-        this.displayValue = String(roundN(Number(this.displayValue), 10));
+        this.displayValue = this.roundN(Number(this.displayValue), 10).toString();
         this.display.textContent = this.displayValue;
     }
 
-    handleOperandInput(button) {
+    handleOperandInput(buttonValue, buttonId) {
         if (this.displayValue.length >= 10) { return; }
-
-        const buttonValue = button.textContent;
-        if (button.id === "decimal" && !this.displayValue.includes(".")) {
+        if (buttonId === 'decimal' && !this.displayValue.includes('.')) {
             this.displayValue += buttonValue;
-        } else if (this.displayValue === "0") {
+        } else if (this.displayValue === '0') {
             this.displayValue = buttonValue;
         } else {
             this.displayValue += buttonValue;
         }
     }
 
-    handleOperatorInput(button) {
+    handleOperatorInput(buttonValue) {
         if (this.pendingOperand && this.pendingOperator) {
-            this.displayValue = String(this.operate(this.pendingOperator, Number(this.pendingOperand), Number(this.displayValue)));
+            this.displayValue = this.operate(this.pendingOperator, Number(this.pendingOperand), Number(this.displayValue)).toString();
         }
 
-        this.pendingOperator = button.textContent;
+        this.pendingOperator = buttonValue;
         this.pendingOperand = this.displayValue;
         this.displayValue = '0';
     }
 
-    handleControlInput(button) {
-        const buttonValue = button.textContent;
-
-        if (buttonValue === "AC") {
-            this.displayValue = "0";
+    handleControlInput(buttonValue, buttonId) {
+        if (buttonValue === 'AC') {
+            this.displayValue = '0';
             this.pendingOperand = null;
             this.pendingOperator = null;
-        } else if (button.id === "equals") {
-            if (this.pendingOperand && this.pendingOperator) {
-                this.displayValue = String(this.operate(this.pendingOperator, Number(this.pendingOperand), Number(this.displayValue)));
-                this.pendingOperand = null;
-                this.pendingOperator = null;
-            }
+        } else if (buttonId === 'equals' && this.pendingOperand && this.pendingOperator) {
+            this.displayValue = this.operate(this.pendingOperator, Number(this.pendingOperand), Number(this.displayValue)).toString();
+            this.pendingOperand = null;
+            this.pendingOperator = null;
         }
     }
 
     operate(operator, a, b) {
         switch (operator) {
-            case "+":
+            case '+':
                 return this.add(a, b);
-            case "-":
+            case '-':
                 return this.subtract(a, b);
-            case "*":
+            case '*':
                 return this.multiply(a, b);
-            case "/":
+            case '/':
                 return this.divide(a, b);
             default:
                 return a;
@@ -93,14 +87,8 @@ class Calculator {
     add(a, b) { return a + b; }
     subtract(a, b) { return a - b; }
     multiply(a, b) { return a * b; }
-    divide(a, b) {
-        if (b === 0) { return NaN; } return a / b;
-    }
+    divide(a, b) { return b !== 0 ? a / b : NaN; }
+    roundN(value, digits) { return Math.round(value * 10 ** digits) / 10 ** digits; }
 }
 
-function roundN(value, digits) {
-    var tenToN = 10 ** digits;
-    return (Math.round(value * tenToN)) / tenToN;
-}
-
-document.addEventListener("DOMContentLoaded", () => { new Calculator(); });
+document.addEventListener('DOMContentLoaded', () => new Calculator());
