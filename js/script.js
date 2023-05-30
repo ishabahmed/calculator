83c56f5682fd4aaea45f6e7d9d0b73e484f36e35
Ishab <43892687+ishabahmed@users.noreply.github.com>
2023-05-30 11:48:56 +0100

    Add input numbers and clear display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,66 @@
 class Calculator {
     constructor() {
-        this.displayValue = 0;
+        this.displayValue = "0";
+        this.result = 0;
         this.initialize();
     }
 
     initialize() {
-        this.display = document.querySelector("#display-text");
+        this.display = document.querySelector(".calculator__display-text");
+        this.display.textContent = this.displayValue;
+
+        this.setEventListeners();
+    }
+
+    setEventListeners() {
+        const buttons = document.querySelectorAll(".calculator__button");
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                this.handleInput(button);
+            });
+        });
+    }
+
+    handleInput(button) {
+        const buttonType = button.classList.value;
+
+        if (buttonType.includes("operand")) {
+            this.handleOperandInput(button);
+        } else if (buttonType.includes("operator")) {
+            this.handleOperatorInput(button);
+        } else if (buttonType.includes("control")) {
+            this.handleControlInput(button);
+        }
+
         this.display.textContent = this.displayValue;
     }
 
+    handleOperandInput(button) {
+        if (this.displayValue.length >= 10) { return; }
+
+        const buttonValue = button.textContent;
+
+        if (button.id === "decimal") {
+            if (!this.displayValue.includes(".")) {
+                this.displayValue += buttonValue;
+            }
+        } else {
+            if (this.displayValue == "0") {
+                this.displayValue = buttonValue;
+            } else {
+                this.displayValue += buttonValue;
+            }
+        }
+    }
+
+    handleControlInput(button) {
+        const buttonValue = button.textContent;
+
+        if (buttonValue === "AC") {
+            this.displayValue = "0";
+            this.result = 0;
+        }
+    }
 
     add(a, b) {
         return a + b;
@@ -39,6 +91,11 @@ class Calculator {
                 return multiply(a, b);
             case "/":
                 return divide(a, b);
+            case "=":
+                return a;
         }
     }
 }
+
+/* when dom ready */
+document.addEventListener("DOMContentLoaded", () => { new Calculator(); });
